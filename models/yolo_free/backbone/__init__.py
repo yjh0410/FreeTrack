82e0fcbf891101f384852f8e0c8f07643f871754
yjh0410 <1394571815@qq.com>
2022-11-30 20:59:05 +0800

    debug demo.py

--- models/yolo_free/backbone/__init__.py
+++ models/yolo_free/backbone/__init__.py
@@ -2,25 +2,16 @@ from .elannet import build_elannet
 from .shufflenetv2 import build_shufflenetv2
 
 
-def build_backbone(cfg, trainable=False):
+def build_backbone(cfg):
     print('==============================')
     print('Backbone: {}'.format(cfg['backbone'].upper()))
 
-    # imagenet pretrained
-    pretrained = cfg['pretrained'] and trainable
-
     if cfg['backbone'] in ['elannet_large', 'elannet_huge', \
                            'elannet_tiny', 'elannet_nano']:
-        model, feat_dim = build_elannet(
-            model_name=cfg['backbone'],
-            pretrained=pretrained
-        )
+        model, feat_dim = build_elannet(model_name=cfg['backbone'])
     
     elif cfg['backbone'] in ['shufflenetv2_0.5x', 'shufflenetv2_1.0x']:
-        model, feat_dim = build_shufflenetv2(
-            model_size=cfg['backbone'][-4:],
-            pretrained=pretrained
-        )
+        model, feat_dim = build_shufflenetv2(model_size=cfg['backbone'][-4:])
         
     else:
         print('Unknown Backbone ...')
