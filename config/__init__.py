88615e6035ea8786d617984cebb22282da66fe72
yjh0410 <1394571815@qq.com>
2022-11-30 20:57:41 +0800

    debug demo.py

--- config/__init__.py
+++ config/__init__.py
@@ -3,7 +3,7 @@ from .yolo_free_config import yolo_config
 
 def build_config(args):
     print('==============================')
-    print('Config: {} ...'.format(args.version.upper()))
+    print('Config: {} ...'.format(args.detector.upper()))
     
     if args.detector in ['yolo_free_nano', 'yolo_free_tiny',\
                          'yolo_free_large', 'yolo_free_huge']:
