778a6adfb4d7ddefe096c4c624327b167ebea2e0
yjh0410 <1394571815@qq.com>
2022-12-14 14:26:59 +0800

    debug tracker

--- config/__init__.py
+++ config/__init__.py
@@ -2,9 +2,7 @@ from .yolo_free_config import yolo_config
 
 
 def build_config(args):
-    print('==============================')
-    print('Config: {} ...'.format(args.detector.upper()))
-    
+    print('==============================')    
     if args.detector in ['yolo_free_nano', 'yolo_free_tiny',\
                          'yolo_free_large', 'yolo_free_huge']:
         cfg = yolo_config[args.detector]
